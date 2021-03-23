db1ebd69f007b25444d45d59366b7af71ca1c186
Brandon Prange <bprange93@gmail.com>
2021-03-23 12:10:18 -0500

    Gone through and notes added to describe each step. Update way to take out a space if that is ever needed.

--- Palindrome.cs
+++ Palindrome.cs
@@ -9,15 +9,25 @@ namespace Demo_Test_2
     class Palindrome
     {
         public void palindromeTest() {
-            string word;
 
+            //instantiate a string variable
+            string word;
+            //ask for the user to enter a string of some sort
             Console.WriteLine("Enter a word");
+            //set the response as the string to be checked. 
             word = Console.ReadLine();
 
+            //set char[] temp to get an array instantiated, set = to word then use .ToCharArray() to turn the string
+            //into each letter is a number in the array.
             char[] temp = word.ToCharArray();
+            //take that array and reverse each character. 
             Array.Reverse(temp);
+            //instantiate new variable to take in the temp string. 
             string reverseWord = new string(temp);
 
+            //if statement to compare the words to see if palindrome or not.
+            //.ToLower added incase any letter is capitalized so that won't throw a not palindrome
+            //when it is just is a different character according to the computer when it reads. 
             if (word.ToLower().Equals(reverseWord.ToLower()))
             {
                 Console.WriteLine("Palindrome");
