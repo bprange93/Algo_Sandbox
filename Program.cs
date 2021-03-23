632579aff065829289f7a29cb097b93b8900918b
Brandon Prange <bprange93@gmail.com>
2021-03-23 11:21:04 -0500

    Methods run and commented out to run a single method at a time. Left in order to test any modifications to help make challenges better or add variations.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solution solution = new Solution();
+            //solution.runSolution();
+
+            //Palindrome palindrome = new Palindrome();
+            //palindrome.palindromeTest();
+
+            //ReverseString reverseString = new ReverseString();
+            //reverseString.reverseString();
+        }
+    }
+}
