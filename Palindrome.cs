e8048d81e17455f5f1765edd2e577cc3bc411e36
Brandon Prange <bprange93@gmail.com>
2021-03-23 11:07:50 -0500

    Class created in order to practice Palindrome.

---
+++ Palindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Test_2
+{
+    class Palindrome
+    {
+        public void palindromeTest() {
+            string word;
+
+            Console.WriteLine("Enter a word");
+            word = Console.ReadLine();
+
+            char[] temp = word.ToCharArray();
+            Array.Reverse(temp);
+            string reverseWord = new string(temp);
+
+            if (word.ToLower().Equals(reverseWord.ToLower()))
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
