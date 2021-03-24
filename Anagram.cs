eb9e2da70d74ea46201358688259cc9d0611dafd
Brandon Prange <bprange93@gmail.com>
2021-03-24 11:07:14 -0500

    Notes added. Figured out how to do an anagram using Array.Sort. Next to learn how to figure out how many letters to add to make it an anagram.

---
+++ Anagram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Test_2
+{
+    class Anagram
+    {
+        public void anagramTest()
+        {
+            string word1;
+            string word2;
+
+            //ask for words to be tested for anagrams
+            //compare the letters in the word
+            //find a way to tell how many letters to add to make anagram
+            Console.WriteLine("Enter first word for anagram: ");
+            word1 = Console.ReadLine();
+            Console.WriteLine("Enter second word to be compared to first");
+            word2 = Console.ReadLine();
+            char[] temp = word1.ToCharArray();
+            char[] temp1 = word2.ToCharArray();
+            Array.Sort(temp);
+            Array.Sort(temp1);
+
+            string anagramWord = new string(temp);
+            string isAnagram = new string(temp1);
+
+            if (anagramWord.Equals(isAnagram))
+            {
+                Console.WriteLine("Anagram");
+                
+
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
