cc8f7f637d544688008f694a9d3a4bfdecbbabdd
Brandon Prange <bprange93@gmail.com>
2021-03-26 12:55:28 -0500

    New way(possibly faster cleaner) to reverse a string.

---
+++ ReverseStringArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Test_2
+{
+    class ReverseStringArray
+    {
+        public void reverseStringArray(string word)
+        {
+            char[] temp = word.ToCharArray();
+            Array.Reverse(temp);
+            Console.ReadLine();
+        }
+    }
+}
