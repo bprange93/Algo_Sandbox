253cf76c0237e247c669583aaf8da1f8d77eb749
Brandon Prange <bprange93@gmail.com>
2021-04-06 12:52:00 -0500

    Worked with group. Came up with if cases to make method work properly.

--- likesButton.cs
+++ likesButton.cs
@@ -10,21 +10,31 @@ namespace Demo_Test_2
     {
         public static string Likes(string[] name)
         {
-            List<string> names; 
-            //at 0 position no one likes
-            //at 1 position persons name likes this
-            //at 2 position person1 and person 2 like this
-            //at 3 position person1 person2 and person3 like this
-            //at 4+ postition person1 person2 and X others like this
-            name[0] = "no one likes this";
-            for(int i = name.Length; i < 4; i++)
+            string toReturn = "";
+            if (name.Length < 1 || name == null)
             {
-                name[1] = "\nname[1]: likes this";
-
-                
+                toReturn = "no one likes this";
             }
-            
+            if (name.Length == 1)
+            {
+                toReturn = $"{name[0]} likes this";
+            }
+            if (name.Length == 2)
+            {
+                toReturn = $"{name[0]} and {name[1]} like this";
+            }
+            if (name.Length == 3)
+            {
+                toReturn = $"{name[0]}, {name[1]} and {name[2]} like this";
+            }
+            if (name.Length > 3)
+            {
+                toReturn = $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+            }
+            return toReturn;
         }
+
+
 
     }
 }
