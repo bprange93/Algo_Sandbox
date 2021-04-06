fa418ed72307d1a7f3470094cefb73d9acd640f3
Brandon Prange <bprange93@gmail.com>
2021-04-06 13:55:01 -0500

    Added a method to do the same as the Like method but using a switch case. Want to use diverse ways to accomplish things to help in future to refer back to whenever I were to get stumped.

--- likesButton.cs
+++ likesButton.cs
@@ -34,6 +34,18 @@ namespace Demo_Test_2
             return toReturn;
         }
 
+        public static string switchLikes(string[] name)
+        {
+            switch (name.Length)
+            {
+                case 0: return "no one likes this";
+                case 1: return $"{name[0]} likes this";
+                case 2: return $"{name[0]} and {name[1]}like this";
+                case 3: return $"{name[0]} and {name[1]} and {name[2]} like this";
+                default: return $"{name[0]} and {name[1]} and {name.Length - 2} others like this";
+            }
+        }
+
 
 
     }
