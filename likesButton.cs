5121e485056e9efe41ace89c489aabaab55c166c
Brandon Prange <bprange93@gmail.com>
2021-04-08 16:26:15 -0500

    Found method that uses the push pop stack. Was trying to figure out a way to do this earlier. Took it tested it out and works. Like the way that looks very much.

--- likesButton.cs
+++ likesButton.cs
@@ -111,6 +111,33 @@ namespace Demo_Test_2
             return Recursive(newArray);
         }
 
+        //This is how I wanted to do it using push pop. 
+        public static String[] dirReducPushPop(String[] arr)
+        {
+            Stack<String> stack = new Stack<String>();
+
+            foreach (String direction in arr)
+            {
+                String lastElement = stack.Count > 0 ? stack.Peek().ToString() : null;
+
+                switch (direction)
+                {
+                    case "NORTH": if ("SOUTH".Equals(lastElement)) { stack.Pop(); } else { stack.Push(direction); } break;
+                    case "SOUTH": if ("NORTH".Equals(lastElement)) { stack.Pop(); } else { stack.Push(direction); } break;
+                    case "EAST": if ("WEST".Equals(lastElement)) { stack.Pop(); } else { stack.Push(direction); } break;
+                    case "WEST": if ("EAST".Equals(lastElement)) { stack.Pop(); } else { stack.Push(direction); } break;
+                }
+            }
+            String[] result = stack.ToArray();
+            Array.Reverse(result);
+            foreach(string dir in result)
+            {
+                Console.WriteLine(dir);
+            }
+
+            return result;
+        }
+
 
     }
 }
